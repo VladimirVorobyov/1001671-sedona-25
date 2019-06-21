e45c166333b7e0a38089ea97586c3cb7d0db4594
Вова <worobew1995@mail.ru>
2019-06-21 18:43:48 +0200

    2.0

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,7 @@
 var popup = document.querySelector(".search_form");
 var link = document.querySelector(".search a");
-
-
 popup.classList.add("search_form--hidden");
 link.addEventListener("click", function(evt) {
-     evt.preventDefault();
+    evt.preventDefault();
     popup.classList.toggle('search_form--hidden');
-   
-   
-
-});
+   });
