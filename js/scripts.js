2659dc13f8b6dab7686f15383e9398f7e33d11e2
Вова <worobew1995@mail.ru>
2019-06-21 17:48:01 +0200

    1

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,12 @@
- var link = document.querySelector(".search a");
-        var popup = document.querySelector(".search_form--see");
-        
-        link.addEventListener("click", function(evt) {
-            evt.preventDefault();
-            popup.classList.add('search_form');
-           
-            popup.classList.toggle("search_form--see");
+var popup = document.querySelector(".search_form");
+var link = document.querySelector(".search a");
 
-        });
+
+popup.classList.add("search_form--hidden");
+link.addEventListener("click", function(evt) {
+     evt.preventDefault();
+    popup.classList.toggle('search_form--hidden');
+   
+   
+
+});
